9809cc321914ef262bae86310b00978e4793fca0
James Nelson <104746919+j4655@users.noreply.github.com>
2024-06-24 22:38:01 -0400

    All five response types are now being properly rendered in the surveys and responses are successfully being recorded

--- views.py
+++ views.py
@@ -11,6 +11,7 @@ from .models import Survey_key
 from .models import Response
 # import pandas as pd
 from datetime import datetime
+from urllib.parse import quote, unquote
 
 # Create your views here.
 
@@ -44,7 +45,14 @@ def surveyResponse(request, id):
     response = {'survey_name': survey_key.survey.name,'survey': id, 'key': key, 'questions': []}
     qs_questions = Question.objects.filter(survey_id=id).order_by('id')
     for row in qs_questions:
-      q = {'question_id': row.id, 'question_text': row.text, 'options': row.options, 'response_type': row.response_type.name, 'static_options': row.response_type.static_options.split(',')}
+      option_objects = []
+      if (row.response_type.name == "Options-Single" or row.response_type.name == "Options-Multi"):
+        options_list = row.options.split(',')
+        for i in range(len(options_list)):
+          option_obj = {'option_number': i+1, 'option_text': options_list[i]}
+          option_objects.append(option_obj)
+
+      q = {'question_id': row.id, 'question_text': row.text, 'options': option_objects, 'response_type': row.response_type.name, 'static_options': row.response_type.static_options.split(',')}
       response['questions'].append(q)
     #return JsonResponse(response)
     return render(request, 'surveys/response.html', context=response)
@@ -55,12 +63,25 @@ def submitResponse(request):
   survey = request.POST['survey']
   survey_key = Survey_key.objects.get(survey=Survey.objects.get(pk=survey), key=request.POST['key'])
   data = {'key': [], 'question-number': [], 'value': []}
+  multi_option_data = {'key_map': {}, 'value_map': {}}
   for key, value in request.POST.items():
-    if key[:9] == 'question-':
+    if ( key[:9] == 'question-' and len(key.split('-')) == 2 ):
       data['key'].append(key)
       data['question-number'].append(int(key[9::]))
       data['value'].append(value)
-  
+    elif ( len(key.split('-')) == 3 ):
+        multi_option = key.split('-')
+        multi_option_data['key_map'][multi_option[1]] = key
+        if ( multi_option[1] in multi_option_data['value_map'] ):
+          multi_option_data['value_map'][multi_option[1]] = str(multi_option_data['value_map'][multi_option[1]]) + ',' + quote(str(value))
+        else:
+          multi_option_data['value_map'][multi_option[1]] = quote(str(value))
+  for q in multi_option_data['key_map']:
+    # { question_8 -> key_1, question_8 -> 'this,that,other' }
+    data['key'].append(multi_option_data['key_map'][q])
+    data['question-number'].append(q)
+    data['value'].append(multi_option_data['value_map'][q])
+
   submission = Submission(language='English', ts=datetime.now(), survey_key=survey_key)
   submission.save()
   # return HttpResponse(submission.id)
